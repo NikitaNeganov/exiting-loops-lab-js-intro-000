eb56d8f2daefa86cb0e54bc92756bfa19257e678
Nikita Neganov <nikitaneganov@icloud.com>
2017-10-04 07:16:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,12 @@ function keepGoing(array, changeValue, skipValue){
   }
   return array;
 }
+function findBy(array, findFn){
+  let findValue = null;
+  for (let i = 0; i < array.length; i++){
+    if (array[i] == findFn){
+      findValue = array[i];
+    }
+  }
+  return findValue;
+}
